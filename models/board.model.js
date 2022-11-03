00c01727ed9b780b72d125e05da03a82fa0fcbda
Alan Parmenter <116365548+alanparmeta@users.noreply.github.com>
2022-11-03 21:00:04 +0000

    Create user.model.js with type, description and rating

---
+++ models/board.model.js
@@ -0,0 +1,25 @@
+// model for Board
+const { DataTypes, Model } = require('sequelize')
+const db = require('../db/db')
+
+class Board extends Model{ }
+
+Board.init({ 
+    type: {
+        type: DataTypes.STRING,
+        // primaryKey: true,
+        // autoIncrement: true
+    },
+    description: {
+        type: DataTypes.STRING,
+        // allowNull: false
+    },
+    rating: {
+        type: DataTypes.INTEGER,
+        // primaryKey: true,
+        // autoIncrement: true
+}, {
+    sequelize: db  
+})
+
+module.exports = Board
---
+++ models/user.model copy 2.js
@@ -0,0 +1,21 @@
+// model for user
+const { DataTypes, Model } = require('sequelize')
+const db = require('../db/db')
+
+class User extends Model{ }
+
+User.init({ 
+    name: {
+        type: DataTypes.STRING,
+        // primaryKey: true,
+        // autoIncrement: true
+    },
+    email: {
+        type: DataTypes.STRING,
+        // allowNull: false
+    }
+}, {
+    sequelize: db  
+})
+
+module.exports = User
